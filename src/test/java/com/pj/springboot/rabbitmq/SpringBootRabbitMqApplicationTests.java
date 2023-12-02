81f2ceb91b2c75881b34e12b8264e01895efc83b
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-12-02 02:19:05 -0500

    feat(core): Add project boilerplate
    Added initial project structure for a new Spring Boot application with RabbitMQ.
    Created .gitignore and application properties file.
    Set up basic Maven build with necessary dependencies for Spring Boot, RabbitMQ and testing.
    Added Maven Wrapper for building the project without having installed maven.
    This structure provides the scaffolding needed to start the development process.

---
+++ src/test/java/com/pj/springboot/rabbitmq/SpringBootRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pj.springboot.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
